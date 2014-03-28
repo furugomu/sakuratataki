a19f3d977d49519ed6b8c018e521b1f401d08626
furugomu <furugomu@gmail.com>
2014-03-28 17:50:19 +0900

    画像一覧を別のファイルにした

---
+++ data.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// ここに画像の ID みたいなのを入れると穴から出てくるようになる。
+// SR は背景が透過でないため N と R のみ。
+
+var IMAGE_IDS = [
+  'cca70f56774507cb696b0e85846a8786',
+  'dd194a0d6c9801be6c7ff8401a9abb6a',
+  'ccd71c6162319419c02c942f9a4fc25b',
+  '5aa6b80bdf46181a90452a7c9a3d63d7',
+  'a4d1885668eaa8e900c8f5a23eb08026',
+  '6970f984e6d3f291ff2f448a2b1a12d5',
+  '6d9e8b1a789c3e37cbe870d637e2b03e',
+  '1b806506d65b67adbd6ecd51d47077c3'
+];
--- tataki.js
+++ tataki.js
@@ -69,16 +69,7 @@ Hole.prototype.poka = function(callback) {
 
 // もぐら
 function Sakura() {
-  var ids = [
-    'cca70f56774507cb696b0e85846a8786',
-    'dd194a0d6c9801be6c7ff8401a9abb6a',
-    'ccd71c6162319419c02c942f9a4fc25b',
-    '5aa6b80bdf46181a90452a7c9a3d63d7',
-    'a4d1885668eaa8e900c8f5a23eb08026',
-    '6970f984e6d3f291ff2f448a2b1a12d5',
-    '6d9e8b1a789c3e37cbe870d637e2b03e',
-    '1b806506d65b67adbd6ecd51d47077c3',
-  ];
+  var ids = IMAGE_IDS;
   var url = 'http://125.6.169.35/idolmaster/image_sp/card/quest/'+
     ids[Math.floor(Math.random()*ids.length)] + '.png';
 
